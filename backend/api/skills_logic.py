35b0b9fe4861b93a621fae7fe503e73d19250dc9
Àlex Milian <alexmiliandieguez@gmail.com>
2025-12-09 16:17:16 +0100

    skills

--- backend/api/skills_logic.py
+++ backend/api/skills_logic.py
@@ -1,28 +1,27 @@
-import pandas as pd
+import json
 
-MODEL_PATH = "BackTechCareer/backend/ml/models/skills/skill_dict.json"
-
-df = None
+MODEL_PATH = r"/Users/alexmiliandieguez/code/TechCareer/BackTechCareer/backend/api/skills_logic.py"
 skill_dict = None
 
+class SkillsLogic:
 
-def load_model():
-    global skill_dict
+    def load_model():
+        global skill_dict
 
-    skill_dict = pd.read_json(MODEL_PATH)
+        skill_dict = json.load(open(MODEL_PATH, "r"))
 
-load_model()
+    load_model()
 
 
-def match_jobs(resume):
-    resume = [s.lower() for s in resume]
+    def match_jobs(resume):
+        resume = [s.lower() for s in resume]
 
-    job_match_count = {}
+        job_match_count = {}
 
-    for skill in resume:
-        if skill in skill_dict:
-            for job in skill_dict[skill]:
-                job_match_count[job] = job_match_count.get(job, 0) + 1
+        for skill in resume:
+            if skill in skill_dict:
+                for job in skill_dict[skill]:
+                    job_match_count[job] = job_match_count.get(job, 0) + 1
 
-    ranking = sorted(job_match_count.items(), key=lambda x: x[1], reverse=True)
-    return ranking
+        ranking = sorted(job_match_count.items(), key=lambda x: x[1], reverse=True)
+        return ranking [:10]
