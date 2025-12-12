121816e5f1726021831dbbf7a4e5fb5148e20b4f
LeoChavez-DEV <leochavez.dev@hotmail.com>
2025-12-12 17:05:03 +0100

    login funcional

--- backend/main.py
+++ backend/main.py
@@ -2,17 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api.v1.routes.auth_routes import router as auth_router
-from backend.api.v1.routes.predict_routes import router as predict_router
 
-app = FastAPI(title="TechCareer API")
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix="/api/v1")
-app.include_router(predict_router, prefix="/api/v1")
