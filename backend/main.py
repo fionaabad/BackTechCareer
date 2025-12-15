63d193e603521f2d1ff69a83a92bdaf0f5b1f1ed
Fiona Abad <fionaabad13@gmail.com>
2025-12-15 16:09:52 +0100

    merged ttrubels

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from api.v1.routes.auth_routes import router as auth_router
 from api.v1.routes.predict_routes import router as predict_router
+from api.v1.routes.skills_routes import router as skills_router
 from api.v1.routes.chat_routes import router as chat_router
 
 app = FastAPI()
@@ -17,5 +18,6 @@ app.add_middleware(
 
 app.include_router(auth_router, prefix="/api/v1")
 app.include_router(predict_router, prefix="/api/v1")
+app.include_router(skills_router,  prefix="/api/v1/skills")
 app.include_router(chat_router, prefix="/api/v1")
 
